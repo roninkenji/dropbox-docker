4fe8c9f30ba62b57fdc3caf514ca9f824f9c4b53
Kenneth Tan <8052689+roninkenji@users.noreply.github.com>
2015-09-11 15:45:29 +0800

    tweak startup script

--- myinit.sh
+++ myinit.sh
@@ -5,11 +5,11 @@ DROPBOX_USERID=${DROPBOX_USERID:-99}
 DROPBOX_GROUP=${DROPBOX_GROUP:-users}
 DROPBOX_GROUPID=${DROPBOX_GROUPID:-100}
 
-getent group ${DROPBOX_GROUP} && groupadd -g ${DROPBOX_GROUPID} ${DROPBOX_GROUP}
-getent passwd ${DROPBOX_USER} || useradd -d /dropbox --gid=${DROPBOX_GROUP} --groups=users --uid=${DROPBOX_USERID} ${DROPBOX_USER}
-usermod -d /dropbox -m ${DROPBOX_USER}
+getent group ${DROPBOX_GROUP} 2>&1 > /dev/null || groupadd -g ${DROPBOX_GROUPID} ${DROPBOX_GROUP}
+getent passwd ${DROPBOX_USER} 2>&1 > /dev/null && usermod -d /dropbox 
+getent passwd ${DROPBOX_USER} 2>&1 > /dev/null || useradd -d /dropbox -g${DROPBOX_GROUP} -G users -u ${DROPBOX_USERID} ${DROPBOX_USER}
 
 [ ! -f $HOME/.dropbox-dist/dropboxd ] && cp -rp /usr/local/.dropbox-dist $HOME/.
-[ ! -d $HOME/.dropbox/instance1 ] && exec $HOME/.dropbox-dist/dropboxd
+[ ! -d $HOME/.dropbox/instance1 ] && su - ${DROPBOX_USER} -c "exec $HOME/.dropbox-dist/dropboxd"
 su - ${DROPBOX_USER} -c "/usr/local/bin/dropbox.py start"
 
