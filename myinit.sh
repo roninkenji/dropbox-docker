ec595c5601cd5782336f9f66d7c556023f3e4427
Kenneth Tan <8052689+roninkenji@users.noreply.github.com>
2015-10-05 20:13:25 +0800

    Added Dropbox client upgrade code during startup

--- myinit.sh
+++ myinit.sh
@@ -10,5 +10,6 @@ getent passwd ${DROPBOX_USER} 2>&1 > /dev/null && usermod -d /dropbox -s /bin/ba
 getent passwd ${DROPBOX_USER} 2>&1 > /dev/null || useradd -d /dropbox -g ${DROPBOX_GROUP} -G users -u ${DROPBOX_USERID} -s /bin/bash ${DROPBOX_USER}
 
 [ ! -f /dropbox/.dropbox-dist/dropboxd ] && su -l ${DROPBOX_USER} -c "cp -r /usr/local/.dropbox-dist /dropbox/."
+[ $( /dropbox/.dropbox-dist/VERSION ) != $( sort -rV /dropbox/.dropbox-dist/VERSION /usr/local/.dropbox-dist/VERSION | head -n 1 ) ] && su -l ${DROPBOX_USER} -c "rm -fr /dropbox/.dropbox-dist/*; cp -r /usr/local/.dropbox-dist/* /dropbox/.dropbox-dist"
 su -l ${DROPBOX_USER} -c "/dropbox/.dropbox-dist/dropboxd"
 
